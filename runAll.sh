2b91370c47be31f83cf8d9099d1e9c8386ad476e
abombin <abombin93@gmail.com>
2022-02-23 19:04:50 +0000

    multiple taxa

--- runAll.sh
+++ runAll.sh
@@ -1,16 +1,21 @@
 conda activate bactopia
 
-(cd ./input && ls *_R1.fastq.gz) > inputList.txt
+# Initial set up step
+mkdir input
+mkdir process
+mkdir bactopia
+
+(cd ./input && ls *_1.fastq.gz) > inputList.txt
 
 cat inputList.txt |cut -f1 -d"_" > newdir.txt
 
-mkdir process
+
 
 for i in $(cat newdir.txt); do mkdir process/"$i"; cp ./input/"$i"* ./process/"$i"/; done
 
-
 cd process
 
+ Trim and filter the sequences
 sh ../programs/trimmomatic.sh
 sh ../programs/spades.sh
 sh ../programs/camitax.sh
