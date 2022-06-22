4425c37417ab1fb04706179b265bb10fcf716f96
abombin <abombin93@gmail.com>
2022-06-22 17:43:00 -0400

    added function to check if the match can be found in dictionary

--- programs/draft0.py
+++ programs/draft0.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-gtdbDat=pd.read_csv('C:\Users\abomb\OneDrive - Emory University\bacteria\data\gtdb\ar53_metadata_r207.tsv', sep='\t')
+#gtdbDat=pd.read_csv('C:/Users/abomb/OneDrive - Emory University/bacteria/data/gtdb/bac120_metadata_r207.tsv', sep='\t')
 
-print(gtdbDat.columns)
+#gtdbTax=gtdbDat[['accession', 'gtdb_taxonomy', 'ncbi_organism_name']]
+
+#pd.DataFrame.to_csv(gtdbTax, 'bac120_metadata_r207Filtr.tsv', index=False, sep='\t')
+
+gtdbDat=pd.read_csv('C:/Users/abomb/OneDrive - Emory University/bacteria/data/gtdb/bac120_metadata_r207Filtr.tsv', sep='\t')
+
+sample=gtdbDat.loc[gtdbDat['gtdb_taxonomy']=='d__Bacteria;p__Proteobacteria;c__Gammaproteobacteria;o__Enterobacterales;f__Enterobacteriaceae;g__Serratia;s__Serratia marcescens_B']
+
+print(sample.head(10))
--- programs/findMismatchGtdk.py
+++ programs/findMismatchGtdk.py
@@ -1,7 +1,11 @@
 
 import pandas as pd
+# gtdb summary
 df=pd.read_csv("data/gtdbtk.bac120.summary.tsv",sep='\t', converters={'user_genome': lambda x: str(x)})
+# original metadata
 metadat=pd.read_csv("data/metaDat.csv", converters={'uuid': lambda x: str(x)})
+# gtdb NCBI dictionary
+gtdbDat=pd.read_csv('C:/Users/abomb/OneDrive - Emory University/bacteria/data/gtdb/gtdbtkNcbiDict.tsv', sep='\t')
    
 # get gtdbtk species names
 def getGtdbSpec(df):
@@ -29,15 +33,33 @@ def mergeDat():
     mergedDat=pd.merge(df1, df2, left_on='uuid', right_on='uuid', how='left')
     return mergedDat
 
+# get samples that match metadata
 def findMatched():
     df=mergeDat()
     matched=df.loc[df['Gtdb_taxa']==df['Original_taxa']]
     return matched
 
+# get samples that mismatch metadata
 def findMismatch():
     df=mergeDat()
     mismatched=df.loc[df['Gtdb_taxa']!=df['Original_taxa']]
     return mismatched
+
+# check if mismatch was due to misspelling
+def checkDictionary():
+    df=findMismatch()
+    df['Ncbi_match']=''
+    for i in range(0, len(df.index)):
+        taxa=df['Gtdb_taxa'][i]
+        dfMatch=gtdbDat[gtdbDat['gtdb_taxonomy'].str.contains(taxa)].reset_index()
+        if df['Original_taxa'][i]==dfMatch['ncbi_organism_name'][0]:
+            df['Ncbi_match'][i]=dfMatch['ncbi_organism_name'][0]
+        else:
+            df['Ncbi_match'][i]=='No_match'
+    return df
+
+print(checkDictionary())
+
 
 
 
