901ec8e546a86e4a3b6b6bf50131f7f96e17fcfb
abombin <abombin93@gmail.com>
2022-06-24 17:00:00 -0400

    reorganized functions, extract samples that were misspelled

--- programs/draft0.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("data/gtdbtk.bac120.summary.tsv",sep='\t', converters={'user_genome': lambda x: str(x)})
-
-for i in df.columns:
-    print(i)
--- programs/findMismatchGtdk.py
+++ programs/findMismatchGtdk.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 # gtdb summary
 df=pd.read_csv("data/gtdbtk.bac120.summary.tsv",sep='\t', converters={'user_genome': lambda x: str(x)})
@@ -6,9 +5,9 @@ df=pd.read_csv("data/gtdbtk.bac120.summary.tsv",sep='\t', converters={'user_geno
 metadat=pd.read_csv("data/metaDat.csv", converters={'uuid': lambda x: str(x)})
 # gtdb NCBI dictionary
 gtdbDat=pd.read_csv('C:/Users/abomb/OneDrive - Emory University/bacteria/data/gtdb/gtdbtkNcbiDict.tsv', sep='\t')
-   
+
 # get gtdbtk species names
-def getGtdbSpec(df):
+def getGtdbSpec():
     phyDat=df[['classification']]
     splitPhyDat=phyDat['classification'].str.split(';', expand=True)
     splitPhyDatRen=splitPhyDat.rename(columns={splitPhyDat.columns[6]: 'Gtdb_taxa'})
@@ -19,33 +18,44 @@ def getGtdbSpec(df):
     gtdbTax = gtdbTax.rename(columns={'user_genome': 'uuid'})
     return gtdbTax
 
+# get alternative taxa
+def getAltTax():
+    phyDat=df[['other_related_references(genome_id,species_name,radius,ANI,AF)']]
+    splitPhyDat=phyDat['other_related_references(genome_id,species_name,radius,ANI,AF)'].str.split(',', expand=True)\
+        .rename({ 1: 'Alt_taxa'}, axis=1)
+    expTax=splitPhyDat[['Alt_taxa']]
+    expTax['Alt_taxa']=expTax['Alt_taxa'].str.replace('s__', '')
+    gtdbTax=pd.concat([df[['user_genome']], expTax], axis=1)
+    gtdbTax = gtdbTax.rename(columns={'user_genome': 'uuid'})
+    return gtdbTax
+
 # get species names from original metadata
-def getMetadatSpec(metadat):
+def getMetadatSpec():
     metadatSpec=metadat[['uuid', 'Organism']]
     metadatSpec=metadatSpec.rename(columns={'Organism':'Original_taxa'})
     metadatSpec['Original_taxa']=metadatSpec['Original_taxa'].str.replace('Clostridium difficile', 'Clostridioides difficile')
     return metadatSpec
 
 # merge gtdbtk and original data
-def mergeDat():
-    df1=getGtdbSpec(df)
-    df2=getMetadatSpec(metadat)
+def mergeDat(df1, df2):
     mergedDat=pd.merge(df1, df2, left_on='uuid', right_on='uuid', how='left')
     return mergedDat
 
+# merge main gtdb taxa and alternative taxa with metadata 
+mainMerge=mergeDat(df1=getGtdbSpec(), df2=getMetadatSpec())
+altMerge=mergeDat(df1=getAltTax(), df2=getMetadatSpec())
+
 # get samples that match metadata
 def findMatched():
-    df=mergeDat()
+    df=mainMerge
     matched=df.loc[df['Gtdb_taxa']==df['Original_taxa']]
     return matched
 
 # get samples that mismatch metadata
 def findMismatch():
-    df=mergeDat()
+    df=mainMerge
     mismatched=df.loc[df['Gtdb_taxa']!=df['Original_taxa']]
     return mismatched
-
-# check if mismatch was due to misspelling
 
 def checkDictionary(dfTab, colName):
     dfTab['Ncbi_match']=''
@@ -58,17 +68,15 @@ def checkDictionary(dfTab, colName):
             dfTab['Ncbi_match'][i]=='No_match'
     return dfTab
 
-def getAltTax():
-    phyDat=df[['other_related_references(genome_id,species_name,radius,ANI,AF)']]
-    splitPhyDat=phyDat['other_related_references(genome_id,species_name,radius,ANI,AF)'].str.split(',', expand=True)\
-        .rename({ 1: 'Alt_taxa'}, axis=1)
-    expTax=splitPhyDat[['Alt_taxa']]
-    expTax['Alt_taxa']=expTax['Alt_taxa'].str.replace('s__', '')
-    gtdbTax=pd.concat([df[['user_genome']], expTax], axis=1)
-    gtdbTax = gtdbTax.rename(columns={'user_genome': 'uuid'})
-    return gtdbTax
+#print(checkDictionary(dfTab=findMismatch(), colName='Gtdb_taxa'))
 
-print(checkDictionary(dfTab=findMismatch(), colName='Gtdb_taxa'))
+# extract species that mismatched due to spelling
+def extractDictMatch():
+    df=checkDictionary(dfTab=findMismatch(), colName='Gtdb_taxa')
+    matched=df.loc[df['Ncbi_match']!='No_match']
+    return matched
+
+print(extractDictMatch())
 
 
 
